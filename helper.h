132544bc83387f3d27cc0515762b2bbe54f07c17
Xuemeng Wei <77300961+weixuemeng@users.noreply.github.com>
2023-03-09 22:31:49 -0700

    Add files via upload

---
+++ helper.h
@@ -0,0 +1,10 @@
+#ifndef HEADER_FILE2
+#define HEADER_FILE2
+double get_time();
+void c_ask(int id);
+void c_receive(int id, int amount);
+void p_work( int amount);
+void p_sleep(int amount);
+void c_complete(int id, int amount);
+void p_end();
+#endif
---
+++ tands.h
@@ -0,0 +1,5 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+void Trans( int n );
+void Sleep( int n );
+#endif
